f79f43829fd96b6ec1cab70f09d4785e563084fc
anasmahmoud12 <es.anas.mahmoud2023@alexu.edu.eg>
2026-05-08 14:46:47 +0300

    make generate_facts function which will generate facts from knowledge_base

---
+++ api/services/prolog_interact/generate_facts.py
@@ -0,0 +1,23 @@
+from api.knowledge.knowledge_base import COURSE_CATALOG, PREREQUISITE_GRAPH
+
+class GenerateFacts:
+
+ def generate_system_facts():
+    
+    facts = []
+    
+    for course, details in COURSE_CATALOG.items():
+        facts.append(f"course({course}).")
+        facts.append(f"course_difficulty({course}, {details['difficulty']}).")
+        facts.append(f"course_topic({course}, {details['topic']}).")
+
+    for target_course, prerequisites in PREREQUISITE_GRAPH.items():
+        for prereq in prerequisites:
+            facts.append(f"prerequest({prereq}, {target_course}).")
+
+    courses_with_prereqs = PREREQUISITE_GRAPH.keys()
+    for course in COURSE_CATALOG.keys():
+        if course not in courses_with_prereqs:
+            facts.append(f"not_have_prerequest({course}).")
+
+    return " ".join(facts)
