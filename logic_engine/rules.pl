5790ef156fb38096fb0d457587156fff3b260921
anasmahmoud12 <es.anas.mahmoud2023@alexu.edu.eg>
2026-05-08 12:48:07 +0300

    Fix and update prolog engine

--- logic_engine/rules.pl
+++ logic_engine/rules.pl
@@ -1,18 +1,63 @@
-course(cs101)
-course(cs201)
-course(cs301)
+%%
+input from user is his name 
+his course he take 
+level of course_difficulty he want  this not rule 
+what course he student_interest
+but other three ones will be facts i will add in file 
 
-course_difficulty(cs101, easy).
-course_difficulty(cs201, medium).
-course_difficulty(cs301, hard).
+facts we will add 
 
-course_topic(cs101, programming).
-course_topic(cs201, software_engineering).
-course_topic(cs301, ai).
+% student(anas).
+
+% student_interest(anas,machine_langauge).
 
 
-recommend(Student, Course) :-
-    student_preference(Student, Difficulty),
-    student_interest(Student, Topic),
-    course_difficulty(Course, Difficulty),
-    course_topic(Course, Topic).
+% has_finish(anas,programming1).
+
+ 
+
+
+
+%%
+
+course(ai).
+course(programming1).
+course(software_engineering).
+course(programming2).
+course(machine_langauge).
+
+course_difficulty(machine_langauge,medium).
+course_difficulty(ai, hard).
+course_difficulty(software_engineering, medium).
+course_difficulty(programming1, easy).
+course_difficulty(programming2,hard).
+
+
+
+prerequest(machine_langauge,ai).
+prerequest(programming1,programming2).
+prerequest(programming1,software_engineering).
+
+not_have_prerequest(programming1).
+not_have_prerequest(machine_langauge).
+
+% this will be injected from user    
+has_finish(Student,Course).
+%%%%%%%%%%%%%%%%%%%%%%%%% if there is thing 
+
+
+student_learn(Student,Course) :- 
+   has_finish(Student,Course).
+
+
+student_learn(Student,Course):-
+    prerequest(Course,Z),
+    student_learn(Student,Z).
+    
+
+recommend(Student, Course,UserDifficulty) :-
+    student(Student),
+    course(Course),
+    course_difficulty(Course,UserDifficulty),
+    student_interest(Student,Course),
+   (prerequest(Z, Course) -> student_learn(Student, Z) ; not_have_prerequest(Course)).
