42b5c3813f876a4bbc13de69cf93106458f184ef
anasmahmoud12 <es.anas.mahmoud2023@alexu.edu.eg>
2026-05-08 03:43:17 +0300

    make logic prolog files which have rules and facts which will be used in no AI version

---
+++ logic_engine/rules.pl
@@ -0,0 +1,14 @@
+course_difficulty(cs101, easy).
+course_difficulty(cs201, medium).
+course_difficulty(cs301, hard).
+
+course_topic(cs101, programming).
+course_topic(cs201, software_engineering).
+course_topic(cs301, ai).
+
+
+recommend(Student, Course) :-
+    student_preference(Student, Difficulty),
+    student_interest(Student, Topic),
+    course_difficulty(Course, Difficulty),
+    course_topic(Course, Topic).
