bb257063b22fd45430bff6e5961c75fe0fb9e0e3
anasmahmoud12 <es.anas.mahmoud2023@alexu.edu.eg>
2026-05-08 14:34:26 +0300

    make the course knowlage the spects of eacy subject and prerequest graph

---
+++ api/knowledge_base.py
@@ -0,0 +1,16 @@
+from api.difficulty import Difficulty
+from api.topic import Topic
+
+COURSE_CATALOG = {
+    "programming1": {"difficulty": Difficulty.EASY.value, "topic": Topic.PROGRAMMING.value},
+    "software_engineering": {"difficulty": Difficulty.MEDIUM.value, "topic": Topic.SOFTWARE_ENGINEERING.value},
+    "machine_language": {"difficulty": Difficulty.MEDIUM.value, "topic": Topic.PROGRAMMING.value},
+    "ai": {"difficulty": Difficulty.HARD.value, "topic": Topic.AI.value},
+    "programming2": {"difficulty": Difficulty.HARD.value, "topic": Topic.PROGRAMMING.value}
+}
+
+PREREQUISITE_GRAPH = {
+    "ai": ["machine_language"],                 
+    "programming2": ["programming1"],           
+    "software_engineering": ["programming1"]   
+}
